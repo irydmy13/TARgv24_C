975cc4ff46fe5e56f29619c2d4e6a755e60048b1
irydmy13 <kotiukir@gmail.com>
2025-04-23 17:36:08 +0300

    5 osa. ulesanne 3

---
+++ 5 osa ulesanne/Uus ulesanne 3/Opilane.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TARgv24_C._5_osa_ulesanne._Uus_ulesanne_3
+{
+    internal class Opilane
+    {
+        public string Nimi;                        // имя ученика
+        public List<int> Hinded = new List<int>(); // список оценок
+
+        public double ArvutaKeskmine()             // расчёт средней оценки
+        {
+            int summa = 0;
+            foreach (int hinne in Hinded)
+            {
+                summa += hinne;
+            }
+            return (double)summa / Hinded.Count;
+        }
+    }
+}
+    
---
+++ 5 osa ulesanne/Uus ulesanne 3/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TARgv24_C._5_osa_ulesanne._Uus_ulesanne_3
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            List<Opilane> õpilased = new List<Opilane>();
+
+            for (int i = 0; i < 3; i++)
+            {
+                Opilane o = new Opilane();
+
+                Console.Write("Sisesta õpilase nimi: ");
+                o.Nimi = Console.ReadLine();
+
+                Console.WriteLine("Sisesta 3 hinnet:");
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write("Hinne: ");
+                    int hinne = int.Parse(Console.ReadLine());
+                    o.Hinded.Add(hinne);
+                }
+
+                õpilased.Add(o);
+            }
+
+            Console.WriteLine("\n--- Õpilaste keskmised hinded ---");
+
+            double maxKeskmine = 0;
+            string parim = "";
+
+            foreach (Opilane o in õpilased)
+            {
+                double keskmine = o.ArvutaKeskmine();
+                Console.WriteLine($"{o.Nimi} – keskmine hinne: {keskmine:F2}");
+
+                if (keskmine > maxKeskmine)
+                {
+                    maxKeskmine = keskmine;
+                    parim = o.Nimi;
+                }
+            }
+
+            Console.WriteLine($"\nParim õpilane on: {parim} keskmise hindega {maxKeskmine:F2}");
+        }
+    }
+}
+
+
